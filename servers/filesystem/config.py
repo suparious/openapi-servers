52dee331951d84308a5a4d0832caa67d1389ed75
Shaun Prince <suparious@solidrust.net>
2025-06-06 23:34:59 -0700

    updating compose environment

--- servers/filesystem/config.py
+++ servers/filesystem/config.py
@@ -1,7 +1,6 @@
-import os
-import pathlib
-
 # Constants
 ALLOWED_DIRECTORIES = [
-    str(pathlib.Path(os.path.expanduser("~/tmp")).resolve())
-]  # 👈 Replace with your paths
+    "/tmp",
+    "/scratch-space",
+    "/repos"
+]
